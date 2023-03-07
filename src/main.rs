537e6062da70bdab9b9214a06945f50b8250646a
Cleggatron <david.robert.clegg@gmail.com>
2023-03-07 19:31:47 +0000

    added rng

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::io;
 
+use rand::Rng;
+
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+
     println!("Guess the number!");
-    
     println!("Please enter your guess.");
 
     let mut guess = String::new();
